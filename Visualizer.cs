b64d3831e7bae74a6e45381bc4c909af67573cfa
partybusiness <rrh@horriblepain.com>
2019-06-21 21:04:36 -0500

    Made an audio visualizer

---
+++ Visualizer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Visualizer : MonoBehaviour {
+
+    private AudioSource audie;
+
+    [SerializeField]
+    private FFTWindow fftWindow;
+
+    private float[] samples;
+
+    [SerializeField]
+    Material mat;
+
+	void Start () {
+        audie = GetComponent<AudioSource>();
+        samples = new float[64];
+	}
+	
+	void Update () {
+        audie.GetSpectrumData(samples, 0, fftWindow);
+        mat.SetFloatArray("_Samples", samples);
+	}
+}
